74c2376b594dcf772656b8dbf11205128dc6d2d5
Erik Gilling <konkers@konkers.net>
2019-05-19 11:13:41 -0700

    Look up reference images relative to exec path.

--- ingest/reference.go
+++ ingest/reference.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	_ "image/png" // Pull in png decoder.
 	"os"
+	"path/filepath"
 )
 
 var referenceImageCache = map[string]image.Image{}
@@ -15,13 +16,34 @@ func getReferenceImage(name string) (image.Image, error) {
 		return img, nil
 	}
 
-	imgFile := fmt.Sprintf("reference/%s.png", name)
-	reader, err := os.Open(imgFile)
-	if err != nil {
+	execPath, err := os.Executable()
+	if ok {
 		return nil, err
 	}
-	defer reader.Close()
+	execDir, _ := filepath.Split(execPath)
 
+	paths := []string{
+		"reference",
+		filepath.Join(execDir, "..", "src", "github.com", "konkers",
+			"lacodex", "ingest", "reference"),
+	}
+
+	imgName := fmt.Sprintf("%s.png", name)
+	var reader *os.File
+	for _, dir := range paths {
+		imgFile := filepath.Join(dir, imgName)
+		reader, err = os.Open(imgFile)
+		if err == nil {
+			break
+		}
+	}
+
+	if reader == nil {
+		return nil, fmt.Errorf("Can't find reference image for %s.  Tried: %#v",
+			name, paths)
+	}
+
+	defer reader.Close()
 	img, _, err = image.Decode(reader)
 	if err != nil {
 		return nil, err
